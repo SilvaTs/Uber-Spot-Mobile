c40a0ddc2641f75c0d326e12686283d4dd311e7e
SilvaTs <danielsilvatsi@outlook.com>
2020-11-15 14:46:22 -0400

    add tela de verificar ponto spot proximo

--- app/src/main/java/br/com/daniel/hackathon/ui/RunningActivity.java
+++ app/src/main/java/br/com/daniel/hackathon/ui/RunningActivity.java
@@ -2,15 +2,40 @@ package br.com.daniel.hackathon.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import br.com.daniel.hackathon.R;
 
 public class RunningActivity extends AppCompatActivity {
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
+        initComponent();
+        openTheTravelScreen();
+    }
+
+    @SuppressLint("WrongViewCast")
+    public void initComponent() {
+        back = findViewById(R.id.imgBackRunning);
+
+    }
+
+    public void openTheTravelScreen() {
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TravelsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
